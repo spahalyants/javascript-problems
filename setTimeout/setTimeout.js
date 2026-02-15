0b9a0d4edb69acdf42cbae903b199e1a78aba95a
Samvel Pahalyants <spahalyants@gmail.com>
2026-02-15 17:13:19 +0300

    setTimeout function

---
+++ setTimeout/setTimeout.js
@@ -0,0 +1,18 @@
+console.log("Before setTimeout");
+
+setTimeout(
+    function(){
+        console.log("Inside setTimeout function")
+              }, 3000);
+
+console.log("After setTimeout");
+
+
+/* 
+
+Before setTimeout
+After setTimeout
+Inside setTimeout function 
+
+*/
+
